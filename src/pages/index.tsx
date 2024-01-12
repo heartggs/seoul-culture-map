4bdeb25f1e92219d5c27e55b5f69cfc71dbb2f9b
heartggs <jounghj0806@naver.com>
2024-01-12 18:32:21 +0900

    데이터패칭

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,28 @@ import Map from "../component/Map";
 import Markers from "../component/Markers";
 import StoreBox from "../component/StoreBox";
 
-import * as stores from "../data/store_data.json";
+import { StoreType } from "@/interface";
 
-export default function Home() {
+export default function Home({ stores }: { stores: StoreType[] }) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
-  const storeDatas = stores["DATA"];
-
-  console.log(currentStore);
 
   return (
     <>
       <Map setMap={setMap} />
-      <Markers
-        storeDatas={storeDatas}
-        map={map}
-        setCurrentStore={setCurrentStore}
-      />
+      <Markers stores={stores} map={map} setCurrentStore={setCurrentStore} />
       <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const stores = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  ).then((res) => res.json());
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
