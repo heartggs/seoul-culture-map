2dee6d07fa354ee9974e48dba847bf8ccacb1db7
heartggs <jounghj0806@naver.com>
2024-01-13 18:52:46 +0900

    페이지네이션 구현

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -9,3 +9,10 @@ export interface StoreType {
   storeType?: string | null;
   foodCertifyName?: string | null;
 }
+
+export interface storeApiResponse {
+  data: StoreType[];
+  totalPage?: number;
+  totalCount?: number;
+  page?: number;
+}
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
-import { StoreType } from "@/interface";
+import { StoreType, storeApiResponse } from "@/interface";
 
 import { useQuery } from "react-query";
 
 import axios from "axios";
 import Loading from "../../component/Loading";
+import { useRouter } from "next/router";
+
+import Pagination from "../../component/Pagination";
 
 export default function StoreListPage() {
+  const router = useRouter();
+  const { page = "1" }: any = router.query;
+
+  console.log(page);
+
   const {
     isLoading,
     isError,
     data: stores,
-  } = useQuery("stores", async () => {
-    const { data } = await axios("/api/stores");
-    return data as StoreType[];
+  } = useQuery(`stores-${page}`, async () => {
+    const { data } = await axios(`/api/stores?page=${page}`);
+    return data as storeApiResponse;
   });
+
+  console.log(stores);
 
   if (isError) {
     return (
@@ -30,7 +40,7 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.map((store, index) => (
+          stores?.data?.map((store, index) => (
             <li className="flex justify-between gap-x-6 py-5" key={index}>
               <div className="flex gap-x-4">
                 <Image
@@ -65,6 +75,7 @@ export default function StoreListPage() {
           ))
         )}
       </ul>
+      {stores?.totalPage && <Pagination total={stores.totalPage} page={page} />}
     </div>
   );
 }
