156e23f98f4c8ef7361c4caac581ed115e2c7787
heartggs <jounghj0806@naver.com>
2024-01-14 15:11:42 +0900

    무한스크롤 구현

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,26 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { storeApiResponse } from "@/interface";
+import { storeApiResponse, StoreType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<storeApiResponse>
+  res: NextApiResponse<storeApiResponse | StoreType[]>
 ) {
-  const { page = "1" }: { page?: string } = req.query;
+  const { page = "" }: { page?: string } = req.query;
   const prisma = new PrismaClient();
 
-  const count = await prisma.store.count();
-  const skipPage = parseInt(page) - 1;
-  const stores = await prisma.store.findMany({
-    orderBy: { id: "asc" },
-    take: 10,
-    skip: parseInt(page) * 10,
-  });
+  if (page) {
+    const count = await prisma.store.count();
+    const skipPage = parseInt(page) - 1;
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+      take: 10,
+      skip: parseInt(page) * 10,
+    });
 
-  res.status(200).json({
-    page: parseInt(page),
-    data: stores,
-    totalCount: count,
-    totalPage: Math.ceil(count / 10),
-  });
+    res.status(200).json({
+      page: parseInt(page),
+      data: stores,
+      totalCount: count,
+      totalPage: Math.ceil(count / 10),
+    });
+  } else {
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+    });
+
+    return res.status(200).json(stores);
+  }
 }
